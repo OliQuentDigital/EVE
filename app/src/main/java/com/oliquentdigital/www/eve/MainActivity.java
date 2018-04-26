05315a438954cc296c864413a4e2462833dd85a9
Oliquen <olivier.nella@outlook.fr>
2018-04-26 17:25:51 +0200

    PrePhase

--- app/src/main/java/com/oliquentdigital/www/eve/MainActivity.java
+++ app/src/main/java/com/oliquentdigital/www/eve/MainActivity.java
@@ -2,12 +2,55 @@ package com.oliquentdigital.www.eve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mHello;
+    private EditText mNameInput;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHello = (TextView) findViewById(R.id.activity_main_hello);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mSubmitButton = (Button) findViewById(R.id.activity_main_submit_btn);
+
+        mSubmitButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mSubmitButton.setEnabled(s.toString().length() !=0);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
